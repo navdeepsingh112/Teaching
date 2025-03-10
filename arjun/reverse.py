c3149068d134dff88feda6a7778b0f0883f917ef
arjunmagoo1 <arjunmagoo1@gmail.com>
2025-03-10 07:53:28 -0600

    adding updates to reverse

--- arjun/reverse.py
+++ arjun/reverse.py
@@ -1,12 +1,24 @@
- #reverse 
+#  #reverse 
 
 
-s="abcdef"
+# s="abcdef"
 
-n = len(s)
+# n = len(s)
 
-for i in range(n):
+# for i in range(n):
 
-        print(s[i]) #acceess the strinf=g from front
-        print(s[n-1-i]) #access string from back
+#         print(s[i]) #acceess the strinf=g from front
+        
+        
+#         print(s[n-1-i]) #access string from back
+
+
+
+ #reverse 2 
+
+Name = input("Enter your string to be reversed   :  ")
+
+Reverse = Name[: : -1]
+
+print(" The Reverse of the input is :   " , Reverse)
 
